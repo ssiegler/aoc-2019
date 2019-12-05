4e823d6e11b9f535508a132bf99ae01153cbbd35
Simon Siegler <simon.siegler@gmail.com>
2019-12-06 00:22:20 +0100

    Solve day 1, part 2

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,6 +1,7 @@
 use std::io;
 use std::io::BufRead;
-use std::ops::{Div, Sub};
+use std::iter::successors;
+use std::ops::Div;
 
 fn main() {
     let masses: Vec<u32> = io::stdin()
@@ -16,16 +17,29 @@ fn main() {
     println!(
         "The sum of the fuel requirements is {}",
         summed_fuel_requirements
+    );
+    let total_fuel_required: u32 = masses.iter().cloned().map(total_fuel_required).sum();
+    println!(
+        "The sum of the total fuel requirements is {}",
+        total_fuel_required
     )
 }
 
+fn total_fuel_required(mass: u32) -> u32 {
+    successors(Some(mass), |&mass| Some(fuel_required(mass)))
+        .skip(1)
+        .take_while(|&fuel| fuel > 0)
+        .sum()
+}
+
 fn fuel_required(mass: u32) -> u32 {
-    mass.div(3).sub(2)
+    mass.div(3).saturating_sub(2)
 }
 
 #[cfg(test)]
 mod tests {
     use super::fuel_required;
+    use crate::total_fuel_required;
 
     #[test]
     fn mass_of_12_needs_2_fuel() {
@@ -44,6 +58,20 @@ mod tests {
 
     #[test]
     fn mass_of_100756_needs_33583_fuel() {
-        assert_eq!(33583, fuel_required(100756));
+        assert_eq!(33583, fuel_required(100_756));
+    }
+
+    #[test]
+    fn total_fuel_for_mass_14_is_2() {
+        assert_eq!(2, total_fuel_required(14));
+    }
+
+    #[test]
+    fn total_fuel_for_mass_1969_is_966() {
+        assert_eq!(966, total_fuel_required(1969));
+    }
+    #[test]
+    fn total_fuel_for_mass_100756_is_50346() {
+        assert_eq!(50346, total_fuel_required(100_756));
     }
 }

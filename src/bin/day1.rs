63cac7f1afe2756acd221a0e96b10d78aa0f865a
Simon Siegler <simon.siegler@gmail.com>
2019-12-05 23:54:56 +0100

    Read from stdin

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,34 +1,25 @@
-use std::env;
-use std::fs::File;
-use std::io::{BufRead, BufReader, Result};
+use std::io;
+use std::io::BufRead;
 use std::ops::{Div, Sub};
 
-fn main() -> Result<()> {
-    let masses = read_masses(&determine_input_filename())?;
-
-    let sum: u32 = masses.iter().map(fuel_required).sum();
-    println!("{}", sum);
-    Ok(())
-}
-
-fn determine_input_filename() -> String {
-    env::args().nth(1).expect("Expected input filename")
-}
-
-fn read_masses(filename: &str) -> Result<Vec<u32>> {
-    let file = File::open(filename)?;
-    let file = BufReader::new(file);
-    Ok(file
+fn main() {
+    let masses: Vec<u32> = io::stdin()
+        .lock()
         .lines()
         .map(|line| {
             line.expect("Could not read line")
                 .parse::<u32>()
                 .expect("Could not parse mass")
         })
-        .collect())
+        .collect();
+    let summed_fuel_requirements: u32 = masses.iter().cloned().map(fuel_required).sum();
+    println!(
+        "The sum of the fuel requirements is {}",
+        summed_fuel_requirements
+    )
 }
 
-fn fuel_required(mass: &u32) -> u32 {
+fn fuel_required(mass: u32) -> u32 {
     mass.div(3).sub(2)
 }
 
@@ -38,21 +29,21 @@ mod tests {
 
     #[test]
     fn mass_of_12_needs_2_fuel() {
-        assert_eq!(2, fuel_required(&12));
+        assert_eq!(2, fuel_required(12));
     }
 
     #[test]
     fn mass_of_14_needs_2_fuel() {
-        assert_eq!(2, fuel_required(&14));
+        assert_eq!(2, fuel_required(14));
     }
 
     #[test]
     fn mass_of_1969_needs_654_fuel() {
-        assert_eq!(654, fuel_required(&1969));
+        assert_eq!(654, fuel_required(1969));
     }
 
     #[test]
     fn mass_of_100756_needs_33583_fuel() {
-        assert_eq!(33583, fuel_required(&100756));
+        assert_eq!(33583, fuel_required(100756));
     }
 }
